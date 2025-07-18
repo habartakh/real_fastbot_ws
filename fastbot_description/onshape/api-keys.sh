afff88af6eeb37040fd731f0fd4de2f0aca7c517
habartakh <bartakh.hajar@gmail.com>
2025-07-18 19:15:17 +0000

    Add necessary packages

---
+++ fastbot_description/onshape/api-keys.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Replace access and secret key with your own, copied from the popup once API key was created in onshame:
+# The API key's access key is The API key's access key is ... and the secret key is ... Please transfer this securely to your application now as you will not be able to display this secret key string again. 
+export ONSHAPE_API=https://cad.onshape.com
+export ONSHAPE_ACCESS_KEY=2WDvl7uk9HuxpdyEOyZB1Eqv 
+export ONSHAPE_SECRET_KEY=TeCx0SoBTFiLboGyFIbuu7oycwK765H57K21HvdZOs6SBlWt 
